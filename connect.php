e1ce5aadc278b2c0c358276213310199109a965b
egestasnet <69169958+egestasnet@users.noreply.github.com>
2020-08-26 17:25:50 +0200

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$SQL_host     = '127.0.0.1:3307';
-$SQL_user     = 'developer';
-$SQL_password = 'Fraai_M0ment';
-$SQL_database = 'voting';
-
-$db = new MySQLi( $SQL_host, $SQL_user, $SQL_password, $SQL_database );
-$db->set_charset("utf8mb4");
-$db->query("SET NAMES utf8mb4 COLLATE utf8mb4_general_ci");
